3d14c5c941e8b18e0ea21245a0f769a4f9d57787
jskim <js.story14@gmail.com>
2022-07-13 22:43:29 +0900

    db 접속정보 변경 / 파일삭제

---
+++ src/test/java/com/nb/spring/product/controller/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package com.nb.spring.product.controller;
+
+import com.nb.spring.product.model.vo.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void insertProductEnd() {
+        String maxDate = "2022-06-12";
+        String maxTime = "12:00";
+        String seller = "2";
+        String unit = "5000";
+        String[] multipartFiles = {"123.jpg","jjj.jpg"};
+
+    }
+}
